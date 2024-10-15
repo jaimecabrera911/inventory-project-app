5b9a02ec017a7bbe96eab2802a4da438c04e013b
Jaime Andres Cabrera <46851287+jaimecabrera911@users.noreply.github.com>
2024-10-15 01:00:47 -0500

    Add JwtModule and JwtService to AppModule providers and imports for JWT functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
 import { UserService } from './user/user.service';
 import { ProductModule } from './product/product.module';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, JwtModule],
   controllers: [AppController],
-  providers: [AppService, AuthService, UserService],
+  providers: [AppService, AuthService, UserService, JwtService],
 })
 export class AppModule {}
